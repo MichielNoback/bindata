d1b63f494cbb4b7e1e894ba8f6f0d60422277086
Michiel Noback <m.a.noback@pl.hanze.nl>
2018-09-19 21:10:18 +0200

    datasets britains species and golden gate added

--- R/data.R
+++ R/data.R
@@ -22,3 +22,60 @@
 #' head(snakes)
 "snakes"
 
+#' Bird species on the British isles
+#'
+#' Numbers of bird species observed on 42 British Isles, including area,
+#' latitude information, distance from Britain and soil variation.
+#'
+#' @references
+#'   M.P. Johnson and D.S. Simberloff (1974). "Environmental Determinants
+#'   of Island Species Numbers in the British Isles," Journal of Biogeography,
+#'   Vol. 1, pp. 149-154
+#'
+#'   E.D. McCoy and E.F. Connor (1976). "Enviromnmental Determinants of
+#'   Island Species Number in the British Isles: A Reconsideration,"
+#'   Journal of Biogeography, Vol. 3, pp. 381-382.
+#'
+#' @format A data frame with 42 observations and 7 variables:
+#' \describe{
+#' \item{island}{The name of the island}
+#' \item{area}{The surface area of the island, in square kilometer}
+#' \item{elevation}{Maximum elevation in meters}
+#' \item{soil.types}{The number of different soil types found on the island}
+#' \item{latitude}{Midpoint of latitude range in degrees north}
+#' \item{dis.from.britain}{Distance in kilometers}
+#' \item{num.species}{Number of observed species}
+#' }
+#' @examples
+#' head(britain.species)
+"britain.species"
+
+#' Bird observation
+#'
+#' This dataset contains bird observations of the bird monitoring group called
+#' Golden Gate Audubon Society. They are active in the the San Francisco Bay
+#' area.
+#'
+#' @references
+#' http://goldengateaudubon.org/birding-resources/observations/
+#'
+#' @format A data frame with 2019 observations and 13 variables:
+#' \describe{
+#' \item{Species.No}{An identifier for the species}
+#' \item{Genus}{The genus the bird belongs to}
+#' \item{Species}{The scientific name of the species (without the genus part)}
+#' \item{Common.Name}{The English name of the species}
+#' \item{CBRC.Review}{Whether the bird has been reviewed by the California Bird Records Committee}
+#' \item{Date.start}{The start date of the observation (as character!)}
+#' \item{Date.end}{The last observation date (if any)}
+#' \item{Number}{The number of individuals}
+#' \item{Location}{The location of the observation - i.e. the name of the place}
+#' \item{County}{County}
+#' \item{Observer1}{The first (primary) observer}
+#' \item{Other.Obs}{Other Observer(s), comma-separated}
+#' \item{Notes}{Notes}
+#' }
+#'
+#' @examples
+#' head(bird.observations)
+"bird.observations"
--- create_test_data.R
+++ create_test_data.R
@@ -1,15 +1,37 @@
 data.folder <- "external_data"
 
 create.snake.df <- function() {
-    data.file <- "snakes.csv"
-    path <- file.path(data.folder, data.file)
-    snakes <- read.table(path, sep = ";", header = T)
-    devtools::use_data(snakes, overwrite = TRUE)
+  data.file <- "snakes.csv"
+  path <- file.path(data.folder, data.file)
+  snakes <- read.table(path, sep = ";", header = T)
+  devtools::use_data(snakes, overwrite = TRUE)
+}
+
+create.britain.species.df <- function() {
+  data.file <- "britain_species.txt"
+  path <- file.path(data.folder, data.file)
+  britain.species <- read.table(path, sep = ";", header = T)
+  devtools::use_data(britain.species, overwrite = TRUE)
+}
+
+create.golden.gate.observations.df <- function() {
+  data.file <- "Observations-Data-2014.csv"
+  path <- file.path(data.folder, data.file)
+  #bird.observations <- read.table(path, sep = ";", header = T)
+  bird.observations <- read.table(path,
+                         sep = ";",
+                         head = T,
+                         na.strings = "",
+                         quote = "",
+                         comment.char = "",
+                         as.is = c(1, 6, 7, 8, 13))
+  devtools::use_data(bird.observations, overwrite = TRUE)
 }
 
 create.test.data <- function() {
-    create.snake.df()
+  #create.snake.df()
+  #create.britain.species.df()
+  create.golden.gate.observations.df()
 }
 
 create.test.data()
-
